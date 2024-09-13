35873b2adaf6f293576a5d9faa0f2fa31edf58ef
Sameed Ul Haq <145802784+sameed-haq@users.noreply.github.com>
2024-09-13 17:26:35 +0530

    Add files via upload

---
+++ terraform/version.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    harness = {
+      source = "harness/harness"
+    }
+  }
+}
+
+provider "harness" {
+  endpoint         = "https://qa.harness.io/gateway"
+  account_id       = "Ws0xvw71Sm2YmpSC7A8z4g"
+  platform_api_key = "pat.Ws0xvw71Sm2YmpSC7A8z4g.66a71532fc4a487bcbcbecb6.Q3NobVGcy0OkNQ2pa6Pq"
+}
