3636e0434e8cfd09187e61ba35df44561b4c642a
Sameed Ul Haq <145802784+sameed-haq@users.noreply.github.com>
2024-09-13 18:45:28 +0530

    Update version.tf

--- terraform/version.tf
+++ terraform/version.tf
@@ -1,10 +1,4 @@
-terraform {
-  required_providers {
-    harness = {
-      source = "harness/harness"
-    }
-  }
-}
+
 
 provider "harness" {
   endpoint         = "https://qa.harness.io/gateway"
