474c98e42585dee45996b82d2b0c6dbd0ba0b170
Sameed Ul Haq <145802784+sameed-haq@users.noreply.github.com>
2024-09-25 18:51:14 +0530

    Update main.tf

--- terrafrom3/main.tf
+++ terrafrom3/main.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-provider "harness" {
-  endpoint         = "https://qa.harness.io/gateway"
-  account_id       = "Ws0xvw71Sm2YmpSC7A8z4g"
-  platform_api_key = "pat.Ws0xvw71Sm2YmpSC7A8z4g.66a71532fc4a487bcbcbecb6.Q3NobVGcy0OkNQ2pa6Pq"
-}
-
 resource "harness_platform_policy" "test" {
        description    = "test1123"
        identifier     = "a2111testing"
