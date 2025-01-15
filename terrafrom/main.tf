4fea4e550cf4d3ace838d1a07672b71ce1a07479
Sameed Ul Haq <145802784+sameed-haq@users.noreply.github.com>
2025-01-15 14:01:33 +0530

    Update main.tf

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -1,1 +1,37 @@
+variable "oci_helm_url" {
+  description = "URL of the OCI Helm repository"
+  type        = string
+}
+
+variable "delegate_selectors" {
+  description = "Delegate selectors for the Helm connector"
+  type        = list(string)
+}
+
+variable "oci_helm_credentials" {
+  description = "OCI Helm credentials (optional)"
+  type = object({
+    username     = string
+    password_ref = string
+  })
+  default = null
+}
+
+resource "harness_platform_connector_oci_helm" "oci_helm_connector" {
+  identifier  = "OIC_HELM_BF"
+  name        = "OIC_HELM_BF"
+  description = "OIC_HELM_BF"
+  tags        = ["foo:bear"]
+
+  url                = var.oci_helm_url
+  delegate_selectors = var.delegate_selectors
+
+  dynamic "credentials" {
+    for_each = var.oci_helm_credentials != null ? [1] : []
+    content {
+      username     = var.oci_helm_credentials["username"]
+      password_ref = var.oci_helm_credentials["password_ref"]
+    }
+  }
+}
 
