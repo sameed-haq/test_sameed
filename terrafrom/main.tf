a1cdc4deb8d489274404f6e6379b89d44a4603a6
Sameed Ul Haq <145802784+sameed-haq@users.noreply.github.com>
2025-01-15 15:46:48 +0530

    Update main.tf

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -2,19 +2,18 @@ variable "oci_helm_url" {
   description = "URL of the OCI Helm repository"
   type        = string
 }
+variable "oic_user" {
+  description = "URL of the OCI Helm repository"
+  type        = string
+}
+variable "oic_pass" {
+  description = "URL of the OCI Helm repository"
+  type        = string
+}
 
 variable "delegate_selectors" {
   description = "Delegate selectors for the Helm connector"
   type        = list(string)
-}
-
-variable "oci_helm_credentials" {
-  type        = map(string)
-  description = "OCI Helm credentials (if using username/password authentication)"
-  default = {
-    "username"     = "gfdsa"
-    "password_ref" = "account.buildfarm_cloud_provider_access_key"
-  }
 }
 
 resource "harness_platform_connector_oci_helm" "oci_helm_connector" {
@@ -26,12 +25,9 @@ resource "harness_platform_connector_oci_helm" "oci_helm_connector" {
   url                = var.oci_helm_url
   delegate_selectors = var.delegate_selectors
 
-  dynamic "credentials" {
-    for_each = var.oci_helm_credentials != null ? [1] : []
-    content {
-      username     = var.oci_helm_credentials["username"]
-      password_ref = var.oci_helm_credentials["password_ref"]
-    }
+  credentials {
+    username     = var.oic_user
+    password_ref = var.oic_pass
   }
 }
 
